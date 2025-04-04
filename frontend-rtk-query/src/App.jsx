519f272886ffc305fce98edb57f5fc29255f1e2e
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-04-04 17:26:36 +0530

    included routing and styling imports

--- frontend-rtk-query/src/App.jsx
+++ frontend-rtk-query/src/App.jsx
@@ -1,15 +1,26 @@
 import "./App.css";
+import "./styles/main.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Comments from "./components/Comments";
 import Posts from "./components/Posts";
 import Users from "./components/Users";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div>
-      <h1>Hello Redux-Query</h1>
-      <Posts />
-      <Users />
-      <Comments />
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/users" element={<Users/>}/>
+        <Route path="/posts" element={<Posts/>}/>
+        <Route path="/comments" element={<Comments/>}/>
+      </Routes>
+    </Router>
+    // <div>
+    //   <h1>Hello Redux-Query</h1>
+    //   <Posts />
+    //   <Users />
+    //   <Comments />
+    // </div>
   );
 }
 
