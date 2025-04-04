c1b99d32ec15fa6fa46733486db94320a5473150
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-04-04 17:27:16 +0530

    added update and delete Mutation

--- frontend-rtk-query/src/apiSlices/getMyPostsAPI.js
+++ frontend-rtk-query/src/apiSlices/getMyPostsAPI.js
@@ -1,26 +1,59 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
 export const getMyPostsAPI = createApi({
     reducerPath: "getMyPostsAPI",
     baseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:3000/",
     }),
-    refetchOnFocus: true,
+    // refetchOnFocus: true,//refetching when browser tab or window is changed.
+    tagTypes: ["Post"],
     endpoints: (builder) => ({
-        getPosts: builder.query({query: () => "posts"}),
-        createPosts: builder.mutation({
+        getPosts: builder.query({query: () => "posts",
+            providesTags: ['Post'],
+        }),
+        createPost: builder.mutation({
             query: (newPost) => ({
                 url: "posts",
                 method: "POST",
                 body: newPost,
             }),
+            // async onQueryStarted(newPost, {dispatch, queryFullfilled}) {
+            //     try {
+            //         const {data: createPost} = await queryFullfilled;
+    
+            //         dispatch(
+            //             getMyPostsAPI.util.updateQueryData('getPosts', undefined, (draft) => {
+            //                 draft.push(createPost);
+            //             })
+            //         );
+            //     } catch (error) {
+            //         console.log("Error updating cache", error);
+            //     }
+            // },
+            // invalidatesTags: ['Post'],
         }),
+       
+        updatePost: builder.mutation({
+            query: ({id, ...updatedPost}) => ({
+                url: `posts/${id}`,
+                method: "PATCH",
+                body: updatedPost,
+            }),
+            invalidatesTags: ['Post'],
+        }), 
         
+        deletePost: builder.mutation({
+            query: (id) => ({
+                url : `posts/${id}`,
+                method: "DELETE",
+            })
+        })  
     }),
 });
 
 export const {
     useGetPostsQuery,
     useCreatePostMutation,
+    useUpdatePostMutation,
+    useDeletePostMutation,
 } = getMyPostsAPI;
